41264118e343d0bcd7b3b55b530b2e80b420bf25
mrStefanJ <stefanjoncic92@gmail.com>
2024-08-05 13:10:09 +0200

    footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,18 +3,7 @@ import "./style.css";
 const Footer = () => {
   return (
     <footer className="footer">
-      <div className="footer__contact">
-        <p className="footer__contact-text">
-          Email: <span>stefanjoncic92@gmail.com</span>
-        </p>
-        <p className="footer__contact-text">
-          Phone: <span>+381 64 000 88 17</span>
-        </p>
-        <p className="footer__contact-text">
-          City: <span>Belgrade</span>
-        </p>
-      </div>
-      <p className="footer__text">Desing and CopyWrite by Stefan Joncic 2024</p>
+      <p className="footer__text">Desing and Developed by Stefan Joncic 2024</p>
     </footer>
   );
 };
